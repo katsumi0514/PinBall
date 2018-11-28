ecfc912b55169de5eda44b246a50ca48b940cad7
伊井克海 <mt4.05140901@gamil.com>
2018-11-28 22:10:06 +0900

    レッスン５の課題です。 よろしくお願いします。

---
+++ Assets/Point.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Point : MonoBehaviour {
+	public Text ScoreText;
+	private int score = 0; 
+
+	void Start()
+	{
+		score = 0;
+		SetScore ();  
+	}
+
+		void OnCollisionEnter( Collision other )
+		{
+			string yourTag  = other.gameObject.tag;
+
+			if( yourTag == "SmallStarTag" )
+			{
+				score += 100;
+			}
+			else if(yourTag == "LargeStarTag"){
+
+				score += 200;
+			}
+
+			SetScore();
+		}
+
+	void SetScore(){
+	ScoreText.text = string.Format( "Score:{0}", score );
+		}
+	}
